addee8ff27c9d1849569011abd00cbe546cdb9bf
adelia <adeliaayuavista27@gmail.com>
2024-03-08 14:47:45 +0700

    membuat form headmaster, create dan edit

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\TeacherController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HeadmasterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,12 +14,23 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('/', function () {
-    return view('welcome');
-});
+
+// Route::get('/', function () {
+//     return view('headmasters');
+// });
+// Route::get('/', function () {
+//     return view('edit');
+// });
+
 
 //Contoh Dari penulisan Route Get didalam route web
 // Route::get('/teacher', [TeacherController::class, 'adding_teacher']);
+
+Route::group(['namespace' => 'headmaster'], function () {
+    Route::get('/headmaster', [HeadmasterController::class, 'index']);
+    Route::get('/create', [HeadmasterController::class, 'setting']);
+    Route::post('headmaster/create', [HeadmasterController::class, 'store']);
+});
 
 //Teacher
 Route::prefix('teacher')->group(function () {
