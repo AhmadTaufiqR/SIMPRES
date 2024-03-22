55ab43c67bfce3133bd362e891b426b6990c5f0a
ameliafz <ameliafawwaz29@gmail.com>
2024-03-22 14:29:00 +0700

    Membuat pelajaran

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -2,9 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Course;
 use Illuminate\Http\Request;
 
 class CourseController extends Controller
 {
-    //
+    public function index_course()
+    {
+        $course = Course::all();
+        return view('templates.amel.courses', ['courses' => $course]);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+        ]);
+        $course = Course::create([
+            'name' => $request->input('name'),
+        ]);
+        if ($course) {
+            return redirect('courses')->with('Success', 'Data pelajaran berhasil ditambahkan');
+        } else {
+            var_dump('kosong');
+        }
+    }
+
+    public function update(Request $request, int $id)
+    {
+        $request->validate([
+            'name' => 'required',
+        ]);
+        $course = Course::findOrFail($id);
+        $course -> update([
+            'name' => $request->input('name'),
+        ]);
+        return redirect('/courses')->with('Success', 'Data Palajaran berhasil diubah' );
+    }
+
+
+    public function delete(int $id)
+    {
+        $course = Course::find($id);
+        if (!$course) {
+            return redirect('/courses')->withErrors('Record not found');
+        }
+        $course->delete();
+        return redirect('/courses')->with('Success', 'Data pelajaran berhasil dihapus');
+    }
 }
+
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CourseController;
 use App\Http\Controllers\TeacherController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -63,4 +64,13 @@ Route::group(['namespace' => 'login'], function () {
     Route::post('/reset-password-act', [UserController::class, 'showResetForm_act']);
     Route::get('/register', [UserController::class, 'index_register'])->name('register');
     Route::post('/register', [UserController::class, 'store'])->name('register.store');
+
 });
+
+Route::group(['namespace' => 'courses'], function () {
+    Route::get('/courses', [CourseController::class, 'index_course']);
+    Route::post('/courses_create', [CourseController::class, 'store']);
+    Route::post('courses/{id}/edit', [CourseController::class, 'update']);
+    Route::delete('courses/{id}/delete', [CourseController::class, 'delete'])->name('delete.course');
+});
+
