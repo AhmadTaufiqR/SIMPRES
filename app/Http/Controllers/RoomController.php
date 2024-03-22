80320d9961b40a4e7f396d98c9261ea163f47803
muhammad25rayhan <rebo250703@gmail.com>
2024-03-22 14:45:00 +0600

    crud kelas

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -2,9 +2,56 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Room;
 use Illuminate\Http\Request;
 
 class RoomController extends Controller
 {
-    //
+    public function index()
+    {
+        $rooms = Room::all();
+        return view('templates.Rayhans.room', compact('rooms'));
+    }
+    public function store(Request $request)
+    {
+        // $request->validate([
+        //     'name_class' => 'required|max:2|min:2'|,
+
+        // ]);
+        $room = Room::Create([
+            'name_class' => $request->name_class,
+
+        ]);
+        if ($room) {
+            return redirect('/room')->with('Success', 'Yeeayy!! Data kelas berhasil ditambahkan');
+        } else {
+            return redirect('/room')->withErrors('Data kelas gagal ditambahkan');
+        }
+    }
+    public function update(Request $request, $id)
+    {
+        ($request->name_class);
+        $room = Room::findOrFail($id)->update([
+            'name_class' => $request->name_class,
+
+        ]);
+        if ($room) {
+            return redirect()->back()->with('Success', 'Yeeayy!! Data kelas berhasil diubah');
+        } else {
+            return redirect('/room')->withErrors('Data kelas gagal diubah');
+        }
+    }
+
+
+
+
+    public function hapus(int $id)
+    {
+        $room = Room::find($id);
+        if (!$room) {
+            dd('Record not found');
+        }
+        $room->delete();
+        return redirect()->back()->with('Success', 'Yeeayy!! Data kelas berhasil dihapus');
+    }
 }
